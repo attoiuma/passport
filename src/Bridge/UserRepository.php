19111dace5fd6b7b4f7149e916227e7fb59a67ed
T.Saisyo <saisyo@liberit.jp>
2019-07-28 14:50:20 +0900

    add getUserByUserCredentials

--- src/Bridge/UserRepository.php
+++ src/Bridge/UserRepository.php
@@ -57,4 +57,34 @@ class UserRepository implements UserRepositoryInterface
 
         return new User($user->getAuthIdentifier());
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUserByUserCredentials($username, $password, $grantType, ClientEntityInterface $clientEntity, $guard)
+    {
+        $provider = config('auth.guards.'.$guard.'.provider');
+
+        if (is_null($model = config('auth.providers.'.$provider.'.model'))) {
+            throw new RuntimeException('Unable to determine authentication model from configuration.');
+        }
+
+        if (method_exists($model, 'findForPassport')) {
+            $user = (new $model)->findForPassport($username);
+        } else {
+            $user = (new $model)->where('email', $username)->first();
+        }
+
+        if (! $user) {
+            return;
+        } elseif (method_exists($user, 'validateForPassportPasswordGrant')) {
+            if (! $user->validateForPassportPasswordGrant($password)) {
+                return;
+            }
+        } elseif (! $this->hasher->check($password, $user->getAuthPassword())) {
+            return;
+        }
+
+        return new User($user->getAuthIdentifier());
+    }
 }
